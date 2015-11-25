3f3812ad601f67a367fe59076c853d9a06f7ebcb
Calvin Pritchard <pritchard.calvin@gmail.com>
2015-11-24 21:57:10 -0800

    created ggplot2 graphs for distance to tracks and station

--- src/s5_graphics.R
+++ src/s5_graphics.R
@@ -1,28 +1,57 @@
-# stage 6: presentation graphics
+# stage 5: presentation graphics
+# ss_data, not_ss_data come from running `s5_graphics.Rmd`
 library(ggplot2)
 
-qplot(x = DistToStation, 
-      y = RDistToStation, 
+# Southern Stations
+qplot(x = DistToStation,
+      y = RDistToStation,
       data = ss_data %>%
-        arrange(DistToStation), 
-      geom = "line", 
+        arrange(DistToStation),
+      geom = "line",
       linetype = factor(Year),
       colour = AggBusinessType) +
-  scale_linetype_manual("Year", breaks=c(2002, 2008, 2014), 
-                        values = c("dashed", "longdash", "solid")) +
-  scale_colour_discrete("Business Licence Type") + 
+  scale_linetype_manual("Year", breaks=c(2002, 2008, 2014),
+                        values = c("dotted", "longdash", "solid")) +
+  scale_colour_discrete("Business Licence Type") +
   ylab("Cumulative Business Licences") +
   xlab("Distance to Nearest Station (m)")
-  
-qplot(x = DistToTracks, 
-      y = RDistToStation, 
+
+qplot(x = DistToTracks,
+      y = RDistToTracks,
       data = ss_data %>%
-        arrange(DistToStation), 
-      geom = "line", 
+        arrange(DistToTracks),
+      geom = "line",
       linetype = factor(Year),
       colour = AggBusinessType) +
-  scale_linetype_manual("Year", breaks=c(2002, 2008, 2014), 
-                        values = c("dashed", "longdash", "solid")) +
-  scale_colour_discrete("Business Licence Type") + 
+  scale_linetype_manual("Year", breaks=c(2002, 2008, 2014),
+                        values = c("dotted", "longdash", "solid")) +
+  scale_colour_discrete("Business Licence Type") +
+  ylab("Cumulative Business Licences") +
+  xlab("Distance to Nearest Station (m)")
+
+# Northern Stations
+qplot(x = DistToStation,
+      y = RDistToStation,
+      data = not_ss_data %>%
+        arrange(DistToStation),
+      geom = "line",
+      linetype = factor(Year),
+      colour = AggBusinessType) +
+  scale_linetype_manual("Year", breaks=c(2002, 2008, 2014),
+                        values = c("dotted", "longdash", "solid")) +
+  scale_colour_discrete("Business Licence Type") +
+  ylab("Cumulative Business Licences") +
+  xlab("Distance to Nearest Station (m)")
+
+qplot(x = DistToTracks,
+      y = RDistToTracks,
+      data = not_ss_data %>%
+        arrange(DistToTracks),
+      geom = "line",
+      linetype = factor(Year),
+      colour = AggBusinessType) +
+  scale_linetype_manual("Year", breaks=c(2002, 2008, 2014),
+                        values = c("dotted", "longdash", "solid")) +
+  scale_colour_discrete("Business Licence Type") +
   ylab("Cumulative Business Licences") +
   xlab("Distance to Nearest Station (m)")
